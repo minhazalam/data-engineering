7e8769752719594692b3d9c1d77f1dfb4dc2a4bd
Minhaz Alam <minhalam.learn@gmail.com>
2023-01-14 13:35:00 +0530

    Add format

--- SQL/shirts_activity.sql
+++ SQL/shirts_activity.sql
@@ -1,32 +1,32 @@
 -- Create a new database
-create database shirts_db;
+CREATE DATABASE shirts_db;
 
 -- Show databases
-show databases;
+SHOW DATABASES;
 
 -- Use created database -> shirts_db
-use shirts_db;
+USE shirts_db;
 
 -- Check selected database
-select database();
+SELECT DATABASE();
 
-show tables;
+SHOW TABLES;
 
 -- Create table shirts
-create table shirts(
-	shirt_id int primary key auto_increment,
-    article varchar(200) not null,
-    color varchar(50) not null,
-    shirt_size varchar(3) not null,
-    last_worn int not null
+CREATE TABLE shirts(
+	shirt_id INT PRIMARY KEY AUTO_INCREMENT,
+    article VARCHAR(200) NOT NULL,
+    color VARCHAR(50) NOT NULL,
+    shirt_size VARCHAR(3) NOT NULL,
+    last_worn INT NOT NULL
 );
 
 -- Drop table
-drop table shirts;
-desc shirts;
+DROP TABLE shirts;
+DESC shirts;
 
 -- Insert values in shirts table
-insert into shirts(article,color,shirt_size,last_worn) values
+INSERT INTO shirts(article,color,shirt_size,last_worn) VALUES
 ('t-shirt', 'white', 'S', 10),
 ('t-shirt', 'green', 'S', 200),
 ('polo shirt', 'black', 'M', 10),
@@ -37,33 +37,33 @@ insert into shirts(article,color,shirt_size,last_worn) values
 ('tank top', 'blue', 'M', 15);
 
 -- Show all the rows in shirts table
-select * from shirts;
+SELECT * FROM shirts;
 
 
 -- Select all shirts but only print out article and color
-select article,color from shirts;
+SELECT article,color FROM shirts;
 
 -- Select all medium shirts print out everything but shirt_id
-select article,color,shirt_size,last_worn
-from shirts
-where shirt_size='M';
+SELECT article,color,shirt_size,last_worn
+FROM shirts
+WHERE shirt_size='M';
 
 -- Update all polo shirts change their size to L
-update shirts set shirt_size='L' where article='polo shirt';
-commit;
-select * from shirts;
+UPDATE shirts SET shirt_size='L' WHERE article='polo shirt';
+COMMIT;
+SELECT * FROM shirts;
 
 -- Update shirts last worn 15 days ago and change last_worn to 0
-update shirts set last_worn=0 where last_worn=15;
+UPDATE shirts SET last_worn=0 WHERE last_worn=15;
 
 -- Delete all old shirts, last worn 200 days ago
-delete from shirts where last_worn=200;
+DELETE FROM shirts WHERE last_worn=200;
 
 -- Delete all ank tops
-delete from shirts where article='tank top';
+DELETE FROM shirts WHERE article='tank top';
 
 -- Delete all shirts
-delete from shirts;
+DELETE FROM shirts;
 
 -- Drop the entire shirts table 
-drop table shirts;
+DROP TABLE shirts;
